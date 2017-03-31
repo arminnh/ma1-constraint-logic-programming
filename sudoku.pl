afe787ee413c4e13b315ebc6ef1e3fa1fc14bc14
armin <halilovic.armin@hotmail.com>
2017-04-01 00:22:08 +0200

    managed to copy the positions of numbers in the given Board into our alternative solution

--- sudoku.pl
+++ sudoku.pl
@@ -20,21 +20,31 @@
 solve(ProblemName) :-
 	problem(ProblemName, Board),
 	print_board(Board),
-	sudoku2(Board),
+	sudoku(Board),
 	labeling(Board),
 	print_board(Board).
 
+print_board(Board) :-
+	dim(Board, [N,N]),
+	( for(I,1,N), param(Board,N) do
+	    ( for(J,1,N), param(Board,I) do
+		X is Board[I,J],
+		( var(X) -> write("  _") ; printf(" %2d", [X]) )
+	    ), nl
+	), nl.
 
 sudoku(Board) :-
 	dim(Board, [N2,N2]),
 	N is integer(sqrt(N2)),
 	Board[1..N2,1..N2] :: 1..N2,
+
 	( for(I,1,N2), param(Board,N2) do
 	    Row is Board[I,1..N2],
 	    alldifferent(Row),
 	    Col is Board[1..N2,I],
 	    alldifferent(Col)
 	),
+
 	( multifor([I,J],1,N2,N), param(Board,N) do
 	    ( multifor([K,L],0,N-1), param(Board,I,J), foreach(X,SubSquare) do
 		X is Board[I+K,J+L]
@@ -68,6 +78,8 @@ werkwijze:
     later wat verbeteren want dit lijkt me niet zo proper
 
 
+
+
 dit probleem lijkt op het n-queens probleems:
     bepaalde nummers mogen andere nummers niet raken
     (bv 1 mag niet op zelfde kolom/rij als andere 1'en, maar
@@ -75,49 +87,105 @@ dit probleem lijkt op het n-queens probleems:
 
     misschien mogelijk om union van 9 viewpoints te gebruiken
     om sudoku voor te stellen?
-
 */
 
-sudoku2(Board) :-
-	dim(Board, [N,N]),
-	%N is integer(sqrt(N2)),
-	% Create blocks for the blocks
-	( for(I,1, N), param(Board,N) do
-        sudoqueens_arrays(N,Board, I)
-	).
+solve2(ProblemName) :-
+    problem(ProblemName, Board),
+	print_board(Board),
+    sudoku2(Board, Values),
+    labeling(Values),
+    print_positions(Values).
 
+print_positions(Values) :-
+    dim(Values, [N,N]),
+    ( for(I, 1, N), param(Values, N) do
+        write(I),
+        write(" ->"),
+        ( for(J, 1, N), param(Values, I) do
+            X is Values[I, J],
+            ( var(X) -> write("  _") ; printf(" %2d", [X]) )
+        ), nl
+    ), nl.
+
+sudoku2(Board, NumbersPositions) :-
+    % dimensions of board: N by N => N possible numbers
+    % board has sqrt(N) blocks of N numbers (with dim sqrt(N) by sqrt(N))
+    dim(Board, [N, N]),
+
+    % array of arrays. each distinct number on the board gets an array.
+    % each number is mapped to an array of positions where this number goes
+    dim(NumbersPositions, [N, N]),
+    NumbersPositions[1..N, 1..N] :: 1..N*N,
+
+    SqrtN is integer(sqrt(N)),
+
+    writeln("start sudoku2"),
+    ( foreach(X, ["fak uuuu", "eclipseclp", "why u terminate without message?????\n"]) do
+        writeln(X)
+    ),
+
+    % assign known positions to values in given board
+    ( multifor([Row, Col], 1, N), param(N, Board, NumbersPositions) do
+        Pos is (Row-1) * N + Col,
+        printf("Row: %d,  Col: %d,  Pos: %d,  N: %d\n", [Row, Col, Pos, N]),
+
+		X is Board[Row, Col],
+		( var(X) ->
+            true
+        ;
+            % if Board has a number in this position
+            printf("Need to have position %d in list NumbersPositions[%d] \n", [Pos, X]),
+            Number is NumbersPositions[X],
+            writeln(Number),
+            array_list(Number, NumberList),
+            writeln(NumberList),
+            member(Pos, NumberList)
+        )
+    ),
+    writeln("end sudoku2").
+
+/*
+Solution for 1:
+    3  6  2  8  4  5  1  7  9
+    1  7  5  9  6  3  2  4  8
+    9  4  8  2  1  7  6  3  5
+    7  1  3  4  5  8  9  6  2
+    2  9  6  7  3  1  5  8  4
+    8  5  4  6  2  9  7  1  3
+    4  3  9  5  7  6  8  2  1
+    5  2  7  1  8  4  3  9  6
+    6  8  1  3  9  2  4  5  7
+
+NumbersPositions array (our other viewpoint) then should be:
+Value 1 goes in positions: 7 10 23 29 42 53 63 67 75
+Value 2 goes in positions: 3 16 22 36 37 50 62 65 78
+Value 3 goes in positions: 1 15 23 30 41 54 56 70 76
+Value 4 goes in positions: 5 17 20 31 45 48 55 69 79
+Value 5 goes in positions: 6 12 27 32 43 47 58 64 80
+Value 6 goes in positions: 2 14 26 35 39 49 60 72 73
+Value 7 goes in positions: 8 11 24 27 40 52 59 66 81
+Value 8 goes in positions: 4 18 21 33 44 46 61 68 74
+Value 9 goes in positions: 9 13 19 34 38 51 57 71 77
+*/
 
 % code based on http://eclipseclp.org/examples/queens_simple.ecl.txt
 sudoqueens_arrays(N, Board, Value) :-
-
 	%dim(Board, [N]),
 	write(Value),
 	Board[1..N, 1..N] :: 1..N,
 
-	( for(I,1,N), param(Board,N, Value) do
-	    ( for(J,I+1,N), param(Board,I, Value) do
-	      Board[I] == Value ->
-	    		(
-					Board[I] #\= Board[J],
-	    		Board[I] #\= Board[J]+J-I,
-	    		Board[I] #\= Board[J]+I-J
-					)
-	    )
-	).
-
-	%Board =.. [_|Vars],
+    ( for(I,1,N), param(Board, Value, N) do
+        ( for(J,I+1,N), param(Board, Value, I) do
+            Board[I] == Value -> (
+                Board[I] #\= Board[J],
+                Board[I] #\= Board[J]+J-I,
+                Board[I] #\= Board[J]+I-J
+            )
+        )
+    )
+	%Board =.. [_|Vars], % CLPArray =.. [_|PrologList],
 	%labeling(Vars).
-
-
-print_board(Board) :-
-	dim(Board, [N,N]),
-	( for(I,1,N), param(Board,N) do
-	    ( for(J,1,N), param(Board,I) do
-		X is Board[I,J],
-		( var(X) -> write("  _") ; printf(" %2d", [X]) )
-	    ), nl
-	), nl.
-
+    .
 
 %----------------------------------------------------------------------
 % Sample data
