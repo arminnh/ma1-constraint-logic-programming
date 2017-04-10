f0af56c2be1500e30cfb77f30432f5fd73c55046
Armin <halilovic.armin@hotmail.com>
2017-04-10 20:39:20 +0200

    cleanup of sudoku.pl

--- sudoku.pl
+++ sudoku.pl
@@ -1,3 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GIVEN SUDOKU SOLUTION WITH TRIVIAL VIEWPOINT
+%
+% Viewpoint(X, D)
+% Variables X: sets of rows, sets of columns, sets of blocks
+% Domain D: sets of values 1..N2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %
 % ECLiPSe sample code - Sudoku problem
 %
@@ -24,15 +32,6 @@ solve(ProblemName) :-
 	labeling(Board),
 	print_board(Board).
 
-print_board(Board) :-
-	dim(Board, [N,N]),
-	( for(I,1,N), param(Board,N) do
-	    ( for(J,1,N), param(Board,I) do
-		X is Board[I,J],
-		( var(X) -> write("  _") ; printf(" %2d", [X]) )
-	    ), nl
-	), nl.
-
 sudoku(Board) :-
 	dim(Board, [N2,N2]),
 	N is integer(sqrt(N2)),
@@ -52,86 +51,36 @@ sudoku(Board) :-
 	    alldifferent(SubSquare)
 	).
 
-/*
-Viewpoint(X, D)
-Variables X: sets of rows, sets of columns, sets of blocks
-Domain D: sets of values 1..N2
-
-Another approach
-Variables X: sets of number 1..N2
-Domain D: set of positions
-
-Values = array met N posities van arrays
-         de N posities stellen de N mogelijke getallen voor die
-         in cellen kunnen voorkomen
-         de arrays zijn arrays van posities, elk getal komt even
-         veel voor, dus elke array is even lang
-
-werkwijze:
-    declare Values variabele
-    ken domain to aan values
-    kopieer posities van Board naar Values
-    pas de sudoku regels toe op Values
-    zet de nieuwe berekende Values in Board
-    done
-
-    later wat verbeteren want dit lijkt me niet zo proper
-
-
-
-
-dit probleem lijkt op het n-queens probleems:
-    bepaalde nummers mogen andere nummers niet raken
-    (bv 1 mag niet op zelfde kolom/rij als andere 1'en, maar
-    mag wel met 2, 3, ...)
-
-    misschien mogelijk om union van 9 viewpoints te gebruiken
-    om sudoku voor te stellen?
-*/
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% OUR SUDOKU SOLUTION WITH ANOTHER VIEWPOINT
+%
+% Viewpoint(X, D)
+% Variables X: sets of positions
+% Domain D: set of values 1..N*N
+%
+% Values:
+%     array of N arrays of N values
+%     each possible number in the sudoku problem gets an array
+%     each array for a number contains positions of the sudoku board that that number lies on
+%     all of the numbers appear an equal amount of times, so each array has equal length
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 solve2(ProblemName) :-
     problem(ProblemName, Board),
+
     writeln("Given board:"),
 	print_board(Board),
+
     writeln("Numbers of positions on a 9x9 board:"),
     ( for(I, 1,  81) do
         printf("%4d", [I]),
         ( mod(I, 9, 0) -> printf("\n", []) ; true)
     ),
+
+    writeln('Sudoku2:'),
     sudoku2(Board, Values),
     labeling(Values),
     print_positions(Values).
-
-test2(Number) :-
-    problem(Number, Board),
-    solution2(Number, Positions),
-
-    writeln('Problem:'),
-    print_board(Board),
-    writeln('Solution with other viewpoint:'),
-    print_positions(Positions),
-
-    writeln('Sudoku2:'),
-    sudoku2(Board, Positions).
-
-print_positions(Values) :-
-    dim(Values, [N,N]),
-    ( for(I, 1, N), param(Values, N) do
-        write(I),
-        write(" ->"),
-        ( for(J, 1, N), param(Values, I) do
-            X is Values[I, J],
-            ( var(X) -> write(" _") ; printf("%4d", [X]) )
-        ), nl
-    ), nl.
-
-sudoku3(Board):-
-	dim(Board, [N,N]),
-	dim(Blocks, [N,N]),
-	Blocks[1..N,1..N] :: 1..N
-
-	.
-
 
 sudoku2(Board, NumbersPositions) :-
     % dimensions of board = N by N and there are N possible numbers to be used on the Board
@@ -184,6 +133,45 @@ sudoku2(Board, NumbersPositions) :-
 
     writeln("end sudoku2").
 
+test2(Number) :-
+    problem(Number, Board),
+    solution2(Number, Positions),
+
+    writeln('Problem:'),
+    print_board(Board),
+
+    writeln('Solution with other viewpoint:'),
+    print_positions(Positions),
+
+    writeln('Sudoku2:'),
+    sudoku2(Board, Positions).
+
+sudoku3(Board):-
+	dim(Board, [N,N]),
+	dim(Blocks, [N,N]),
+	Blocks[1..N,1..N] :: 1..N
+	.
+
+print_board(Board) :-
+    dim(Board, [N,N]),
+    ( for(I,1,N), param(Board,N) do
+        ( for(J,1,N), param(Board,I) do
+        X is Board[I,J],
+        ( var(X) -> write("  _") ; printf(" %2d", [X]) )
+        ), nl
+    ), nl.
+
+print_positions(Values) :-
+    dim(Values, [N,N]),
+    ( for(I, 1, N), param(Values, N) do
+        write(I),
+        write(" ->"),
+        ( for(J, 1, N), param(Values, I) do
+            X is Values[I, J],
+            ( var(X) -> write(" _") ; printf("%4d", [X]) )
+        ), nl
+    ), nl.
+
 % sudoku_shift_rows :- replace a board positions by the first position of the row those
 %                      positions are on in a 9x9 sudoku board.
 % example:
@@ -226,52 +214,9 @@ sudoku_shift_cols([X | Tail], N, [X2 | Tail2]) :-
     XX #= X-N,
     sudoku_shift_cols([XX | Tail], N, [X2 | Tail2]).
 
-/*
-Solution for 1:
-    3  6  2  8  4  5  1  7  9
-    1  7  5  9  6  3  2  4  8
-    9  4  8  2  1  7  6  3  5
-    7  1  3  4  5  8  9  6  2
-    2  9  6  7  3  1  5  8  4
-    8  5  4  6  2  9  7  1  3
-    4  3  9  5  7  6  8  2  1
-    5  2  7  1  8  4  3  9  6
-    6  8  1  3  9  2  4  5  7
-
-NumbersPositions array (our other viewpoint) then should be:
-Value 1 goes in positions: 7 10 23 29 42 53 63 67 75
-Value 2 goes in positions: 3 16 22 36 37 50 62 65 78
-Value 3 goes in positions: 1 15 23 30 41 54 56 70 76
-Value 4 goes in positions: 5 17 20 31 45 48 55 69 79
-Value 5 goes in positions: 6 12 27 32 43 47 58 64 80
-Value 6 goes in positions: 2 14 25 35 39 49 60 72 73
-Value 7 goes in positions: 8 11 24 27 40 52 59 66 81
-Value 8 goes in positions: 4 18 21 33 44 46 61 68 74
-Value 9 goes in positions: 9 13 19 34 38 51 57 71 77
-*/
-
-% code based on http://eclipseclp.org/examples/queens_simple.ecl.txt
-sudoqueens_arrays(N, Board, Value) :-
-	%dim(Board, [N]),
-	write(Value),
-	Board[1..N, 1..N] :: 1..N,
-
-    ( for(I,1,N), param(Board, Value, N) do
-        ( for(J,I+1,N), param(Board, Value, I) do
-            Board[I] == Value -> (
-                Board[I] #\= Board[J],
-                Board[I] #\= Board[J]+J-I,
-                Board[I] #\= Board[J]+I-J
-            )
-        )
-    )
-	%Board =.. [_|Vars], % CLPArray =.. [_|PrologList],
-	%labeling(Vars).
-    .
-
-%----------------------------------------------------------------------
-% Sample data
-%----------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SAMPLE DATA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 problem(1, [](
     [](_, _, 2, _, _, 5, _, 7, 9),
@@ -398,6 +343,11 @@ problem(11, [](
     [](_, 3, _, _, 2, _, 9, _, 5),
     [](_, 9, _, _, _, 3, _, 1, _))).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SOLUTIONS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%  Solution for problem 1 in the trivial viewpoint:
 solution(1, [](
     [](3,  6,  2,  8,  4,  5,  1,  7,  9),
     [](1,  7,  5,  9,  6,  3,  2,  4,  8),
@@ -409,6 +359,7 @@ solution(1, [](
     [](5,  2,  7,  1,  8,  4,  3,  9,  6),
     [](6,  8,  1,  3,  9,  2,  4,  5,  7))).
 
+% Solution for problem 1 in our other viewpoint:
 solution2(1, [](
     [](7, 10, 23, 29, 42, 53, 63, 67, 75),
     [](3, 16, 22, 36, 37, 50, 62, 65, 78),
