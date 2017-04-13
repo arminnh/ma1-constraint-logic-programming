df50f0ce5cb98d425ee0f8bf47bd2c884e189a7c
Armin <halilovic.armin@hotmail.com>
2017-04-13 22:42:45 +0200

    filled board & rows different values constraints in CHR

--- sudoku/CHR/sudoku.pl
+++ sudoku/CHR/sudoku.pl
@@ -1,34 +1,81 @@
 :- use_module(library(chr)).
+:- chr_constraint solve/0, solve/1, sudoku/1, print_board/1, print_numbers/1.
+:- chr_constraint diff/2, list_diff/1, list_diff/2, row_different/1, rows_different/1, enum/1.
 
-:- chr_constraint main/1, hello_world/0, solve/1.
+:- op(700, xfx, in).
+:- op(700, xfx, le).
+:- op(700, xfx, eq).
+:- op(600, xfx, '..').
+:- chr_constraint le/2, eq/2, in/2, add/3.
 
-main([X | Tail]) <=> writeln('hello world'), writeln(X), writeln(Tail).
+solve <=> solve(1).
 
-solve(ProblemName) <=> problem(ProblemName, Board), print_board(Board).
+solve(ProblemName) <=>
+    problem(ProblemName, Board),
+    print_board(Board),
+    sudoku(Board),
+    % labeling(Board),
+    writeln("Result:"),
+    print_board(Board).
 
-% solve(ProblemName) :-
-	% problem(ProblemName, Board),
-	% print_board(Board)
-    % find_chr_constraint(),
-	% sudoku(Board),
-	% labeling(Board),
-	% print_board(Board).
-    %.
+diff(X,Y) <=> nonvar(X), nonvar(Y) | X \== Y.
 
-print_numbers([]).
-print_numbers([ Number | Tail ]) :-
-    var(Number),
-    write(" _"),
-    print_numbers(Tail).
-print_numbers([ Number | Tail ]) :-
+list_diff([]) <=> true.
+list_diff([ Val | Vals ]) <=>
+    list_diff(Val, Vals),
+    list_diff(Vals).
+
+list_diff(_, []) <=> true.
+list_diff(Val, [ Val2 | Vals ]) <=>
+    % writeln(["Val: ", Val, ", Val2: ", Val2]),
+    diff(Val, Val2),
+    list_diff(Val, Vals).
+
+rows_different([]) <=> true.
+rows_different([ Row | Rows ]) <=>
+    length(Row, N),
+    makedomains(N, Row),
+    list_diff(Row),
+    enum(Row),
+    % writeln("row done \n"),
+    rows_different(Rows).
+
+%% enum(N,Qs): fill list Qs successively with values from 1..N
+enum([])            <=> true.
+enum([X|R])             <=> number(X) | enum(R).
+enum([X|R]), X in L     <=> member(X,L), enum(R).
+
+%% upto(N,L): L=[1..N]
+upto(0,[]).
+upto(N,[N|L]) :- N>0, N1 is N-1, upto(N1,L).
+
+%% domain(Qs,D): create 'Q in D' constraints for all Q from Qs
+domain([],_).
+domain([Val|Row],D) :- Val in D, domain(Row,D).
+
+%% makedomains(N,Qs): Qs is an N-elem. list, create 'X in [1..N]' constraints
+makedomains(N,Row) :- length(Row,N), upto(N,D), domain(Row,D).
+
+
+sudoku(Board) <=>
+    rows_different(Board),
+    true.
+
+
+print_numbers([]) <=> writeln("").
+print_numbers([ Number | Tail ]) <=>
+    nonvar(Number) |
     write(" "),
     write(Number),
     print_numbers(Tail).
+print_numbers([ _ | Tail ]) <=>
+    write(" _"),
+    print_numbers(Tail).
 
-print_board([]).
-print_board([ Row | Tail ]) :-
+
+print_board([]) <=> writeln("").
+print_board([ Row | Tail ]) <=>
     print_numbers(Row),
-    writeln(""),
     print_board(Tail).
 
 /*
@@ -51,6 +98,8 @@ sudoku(Board) :-
 	    alldifferent(SubSquare)
 	).
 */
+
+% problem(ProblemName, X, Y, Number)
 
 problem(1, [ [_, _, 2, _, _, 5, _, 7, 9],
              [1, _, 5, _, _, 3, _, _, _],
