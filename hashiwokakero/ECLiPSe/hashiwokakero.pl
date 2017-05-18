9435afdbdc2b06e01397d3d61364299f8efb116b
thierryderuyttere <thierryderuyttere@hotmail.com>
2017-05-18 15:17:08 +0200

    failed so hard yet so close

--- hashiwokakero/ECLiPSe/hashiwokakero.pl
+++ hashiwokakero/ECLiPSe/hashiwokakero.pl
@@ -2,6 +2,7 @@
 % https://www.google.be/search?q=constraint+programming+connectivity&ie=UTF-8&sa=Search&channel=fe&client=browser-ubuntu&hl=en&gws_rd=cr,ssl&ei=BtkaWbw8wrBp6LeukAg
 
 :- lib(ic).
+:- lib(grasper).
 % :- import alldifferent/1, sorted/2 from ic_global.
 % :- coroutine.
 :- lib(lists).
@@ -59,6 +60,32 @@ incr_list([J|T], J) :-
 % and the amounts of briges going North, East, South, or West from the position
 hashiwokakero(Board) :-
     dim(Board, [XMax, YMax, 5]), % 6 variables: Amount, N, E, S, W for each position
+
+    ( multifor([X, Y], 1, [XMax, YMax]), param(Board, XMax, YMax) do
+        Amount is Board[X, Y, 1],
+        N is Board[X, Y, 2],
+        E is Board[X, Y, 3],
+        S is Board[X, Y, 4],
+        W is Board[X, Y, 5],
+
+        % if this position is not on the edges of the board, then the amount of bridges
+        % going in one direction needs to equals the amount in the other direction
+        ( X > 1    -> N #= Board[X-1,   Y, 4] ; N = 0 ),
+        ( Y < YMax -> E #= Board[  X, Y+1, 5] ; E = 0 ),
+        ( X < XMax -> S #= Board[X+1,   Y, 2] ; S = 0 ),
+        ( Y > 1    -> W #= Board[  X, Y-1, 3] ; W = 0 ),
+
+        % if this position requires an amount of bridges,
+        % make the sum of all bridges equal this amount
+        ( Amount > 0 ->
+            [N, E, S, W] #:: 0..2,
+            N + E + S + W #= Amount
+            ; % else make sure bridges don't cross each other
+            N = S, E = W,
+            (N #= 0) or (E #= 0)
+        )
+    ),
+
 
     board_islands(Board, Islands),
     board_islands_count(Board, IslandCount),
