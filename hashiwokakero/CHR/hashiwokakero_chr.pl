26705a1483865d56659d6b5d0552b5c4471f0cd8
thierryderuyttere <thierryderuyttere@hotmail.com>
2017-05-27 21:00:28 +0200

    enum

--- hashiwokakero/CHR/hashiwokakero_chr.pl
+++ hashiwokakero/CHR/hashiwokakero_chr.pl
@@ -1,7 +1,7 @@
 :- use_module(library(chr)).
 
 :- chr_constraint solve/1, puzzle_board/1, print_board/0, hashiwokakero/1.
-:- chr_constraint print_row/1, print_pos/1, enum/1, enum_board/1.
+:- chr_constraint print_row/1, print_pos/1, enum/1, enum_board/0.
 :- chr_constraint make_domain/2, make_domains/1, domain_list/1.
 :- chr_constraint islands_board/1, matrix_board/2, create_islands/1, create_empty_board/3.
 :- chr_constraint board/7, create_board/3, output/1, size/1, print_board/2.
@@ -21,12 +21,16 @@
 solve(Number) <=>
     % find the game board
     puzzle_board(Number),
+    upto(DomainList, 2),
+    domain_list(DomainList),
 
     writeln("Given board:"),
-    print_board.
+    print_board,
+
+    enum_board.
 
     % create bridges and set constraints
-    %hashiwokakero(Board),
+    %hashiwokakero_constraints,
     %writeln("kk"),
 
     % do search on variables
@@ -38,7 +42,7 @@ solve(Number) <=>
 
     % print results
     %writeln("Search done:"),
-    %print_board(Board).
+    %print_board.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % RULES USED FOR READING BOARD
@@ -71,9 +75,13 @@ create_empty_board(X,Y, Size) <=> X > Size|
     create_empty_board(1,Y2,Size)
     .
 
-create_empty_board(X,Y, Size) <=> X =< Size|
+domain_list(Domain) \ create_empty_board(X,Y, Size) <=> X =< Size|
     % X Y Amount, N E, S, W
-    board(X,Y, 0, _, _, _, _),
+    board(X,Y, 0, N, E, S, W),
+    N in Domain,
+    E in Domain,
+    S in Domain,
+    W in Domain,
     X2 is X + 1,
     create_empty_board(X2,Y,Size)
     .
@@ -81,31 +89,18 @@ create_empty_board(X,Y, Size) <=> X =< Size|
 create_islands([]) <=>
     true.
 
-create_islands([ [X,Y,Amount] | Islands]), board(X,Y, _ , _ , _ , _,_) <=>
-    board(X,Y, Amount , _ , _ , _,_),
+domain_list(Domain) \ create_islands([ [X,Y,Amount] | Islands]), board(X,Y, _ , _ , _ , _,_) <=>
+    board(X,Y, Amount , N , E , S,W),
+    N in Domain,
+    E in Domain,
+    S in Domain,
+    W in Domain,
     create_islands(Islands).
-    %board(X,Y, Amount, 0, 0, 0, 0),
-    %create_islands(Islands).
 
 size(Size) \ islands_board(Islands) <=>
     create_empty_board(1,1, Size),
     %dim(Board, [Size, Size, 5]),
     create_islands(Islands),
-    % % fill in the island bridge amounts first
-    % ( foreacharg(Island, Islands), param(Board) do
-    %     X is Island[1],
-    %     Y is Island[2],
-    %     Amount is Island[3],
-    %     Board[X, Y, 1] #= Amount
-    % ),
-    %
-    % % then fill in zeros
-    % ( foreacharg(Row, Board) do
-    %     ( foreacharg(Position, Row) do
-    %         Amount is Position[1],
-    %         ( var(Amount) -> Position[1] #= 0 ; true )
-    %     )
-    % )
     true.
 
 % create a usable Board from a matrix that contains the islands
@@ -119,153 +114,6 @@ matrix_board(Matrix, Board) :-
     % )
     true.
 
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % ISLANDS PROCEDURES
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
-% % Count is the amount of islands on the Board
-% board_islands_count(Board, Count) <=>
-%     dim(Board, [XMax, YMax, _]),
-%
-%     Amounts1 is Board[1..XMax, 1..YMax, 1],
-%     flatten(Amounts1, Amounts2),
-%     array_list(Amounts3, Amounts2),
-%
-%     % ( foreachelem(Amount, Amounts3), fromto(List, Out, In, []) do
-%     %     ( Amount > 0 -> Out = [Amount | In] ; Out = In )
-%     % ),
-%     length(List, Count).
-%
-% % Islands is a list of islands that are on the given Board. Done by passing over the entire board. nth1 is used to prevent permutations causing many solutions. TODO: try with 1 simple procedure using cuts instead?
-% board_islands(Board, Islands) <=>
-%     dim(Board, [XMax, YMax, 5]),
-%     board_islands(Board, 1, 1, XMax, YMax, 1, Islands),
-%     board_islands_count(Board, Count),
-%     length(Islands, Count).
-%
-% % Islands is a list of islands that are on the given Board. Done by passing over the entire board. nth1 is used to prevent permutations causing many solutions. TODO: try with 1 simple procedure using cuts instead?
-% board_islands(Board, X, Y, X, Y, Count, Islands) <=>
-%     Amount is Board[X, Y, 1],
-%     ( Amount > 0 ->
-%         nth1(Count, Islands, [X, Y])
-%     ;
-%         true
-%     ).
-%
-% % Islands is a list of islands that are on the given Board. Done by passing over the entire board. nth1 is used to prevent permutations causing many solutions. TODO: try with 1 simple procedure using cuts instead?
-% board_islands(Board, XNext, YNext, XMax, YMax, Count, Islands) <=> XNext =< XMax, YNext =< YMax|
-%
-%     Amount is Board[XNext, YNext, 1],
-%     ( YNext = YMax ->
-%         YNext2 is 1,
-%         XNext2 is XNext + 1
-%     ;
-%         YNext2 is YNext + 1,
-%         XNext2 is XNext
-%     ),
-%
-%     ( Amount > 0 ->
-%         nth1(Count, Islands, [XNext, YNext]),
-%         CountNext is Count + 1
-%     ;
-%         CountNext is Count
-%     ),
-%     board_islands(Board, XNext2, YNext2, XMax, YMax, CountNext, Islands).
-%
-% % Islands is a list of islands on the Board that form a connected set starting from a certain island. Visited is used to prevent visiting islands multiple times.
-% fill_set_visit(Board, X, Y, Islands, Visited) <=>
-%     island_neighbors(Board, X,Y, Neighbors),
-%     %writeln(Neighbors),
-%     length(Neighbors, N),
-%     % writeln(["         fill_set_visit --- getting position: ", [X, Y], " --- ", Neighbors]),
-%
-%     % ( for(I,1,N), param(Board, Islands, Visited, Neighbors) do
-%     %     %writeln(["I", I]),
-%     %     nth1(I, Neighbors, [X1,Y1]),
-%     %
-%     %     nth1(Pos, Islands, [X1,Y1]),
-%     %     nth1(Pos, Visited, HasVisited),
-%     %     % member([X1,Y1], Neighbors),
-%     %     % writeln(["Checking neighbor: ", X1, Y1, "Which has Index: ", Pos, " in visited and has been visited: ", HasVisited, "Visited set: ", Visited ]),
-%     %     % writeln(["Neighbors ",Neighbors]),
-%     %
-%     %     % If it is still a var, we haven't visited this islands yet so let's go and visit it :D yaayyyy :D :D :D i agree!
-%     %     (var(HasVisited) ->
-%     %         HasVisited is 1,
-%     %         fill_set_visit(Board, X1, Y1, Islands, Visited)
-%     %     ;
-%     %         true
-%     %     )
-%     % ),
-%     !.
-%
-% % TODO pls fill in
-% neighbors_count([], 0) <=>
-%     true.
-%
-% % TODO pls fill in
-% neighbors_count([H | T], Count) <=> H > 0 |
-%     neighbors_count(T, C2),
-%     Count is C2 + 1.
-%
-% % TODO pls fill in
-% neighbors_count([_ | T], Count) <=>
-%     neighbors_count(T, C2),
-%     Count is C2.
-%
-% % Neighbors is a list of neighboring islands (not just positions) of position (X, Y) on the Board
-% island_neighbors(Board, X, Y, Neighbors) <=>
-%     List is Board[X, Y, 2..5],
-%     neighbors_count(List, Count),
-%     length(Neighbors, Count),
-%
-%     % ( foreachelem(Direction, [](2, 3, 4, 5)), param(Board, X,Y, Neighbors) do
-%     %     Val is Board[X,Y,Direction],
-%     %     (Val > 0 ->
-%     %         next_pos([X,Y], Direction, NextPos),
-%     %         find_neighbor(Board, NextPos, Direction, Neighbor),
-%     %         member(Neighbor, Neighbors)
-%     %         ;
-%     %         true
-%     %     )
-%     % )
-%     true.
-%
-% % Neighbor is a possible neighbor in a certain direction from position (X, Y) on the Board
-% find_neighbor(Board, [X, Y], Direction, Neighbor) <=>
-%     dim(Board, [XMax, YMax, _]),
-%
-%     X > 0,
-%     X =< XMax,
-%     Y > 0,
-%     Y =< YMax,
-%
-%     Amount is Board[X, Y, 1],
-%     ( Amount > 0 ->
-%         Neighbor = [X, Y]
-%     ;
-%         next_pos([X, Y], Direction, NextPos),
-%         find_neighbor(Board, NextPos, Direction, Neighbor)
-%     ).
-%
-% % TODO: was this wrong?
-% % find_neighbor(_, _, _, _).
-%
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % HELPER PROCEDURES
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% % count the nonvars of a list, assuming that all of the nonvars are at the end of the list
-% count_nonvars([], 0).
-% count_nonvars([ Head | _ ], 0) :-
-%     var(Head).
-% count_nonvars([ Head | Tail ], Count) :-
-%     nonvar(Head),
-%     count_nonvars(Tail, Count2),
-%     Count is Count2 + 1.
-%
 
 size(Size) \ print_board(_,Y) <=> Y > Size|
     nl, nl,
@@ -298,42 +146,12 @@ print_board <=>
     print_board(1,1),
     true.
 
-% print_board(Board) :-
-%     ( foreacharg(Row, Board) do
-%         ( foreacharg(Position, Row) do
-%             Amount is Position[1],
-%             ( Amount > 0 ->
-%                 write(Amount)
-%             ;
-%                 NS is Position[2],
-%                 EW is Position[3],
-%                 ( nonvar(NS), nonvar(EW) ->
-%                     symbol(NS, EW, Char),
-%                     write(Char)
-%                 ;
-%                     write(' ')
-%                 )
-%             )
-%         ),
-%         nl
-%     ),
-%     nl, nl.
-%
+
 symbol(0, 0, ' ').
 symbol(0, 1, '-').
 symbol(0, 2, '=').
 symbol(1, 0, '|').
 symbol(2, 0, '"').
-%
-% next_pos([X, Y], 2, [X2, Y]) :- X2 is X-1. % north
-% next_pos([X, Y], 4, [X2, Y]) :- X2 is X+1. % south
-% next_pos([X, Y], 3, [X, Y2]) :- Y2 is Y+1. % east
-% next_pos([X, Y], 5, [X, Y2]) :- Y2 is Y-1. % west
-%
-% direction(2, "North").
-% direction(3, "East").
-% direction(4, "South").
-% direction(5, "West").
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % RULES USED FOR CONSTRAINTS
@@ -346,20 +164,22 @@ symbol(2, 0, '"').
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % enum(L): assigns values to variables X in L
-enum([])                        <=> true.
-enum([ X | Tail ])              <=> number(X) | enum(Tail).
-enum([ X | Tail ]), X in Domain <=> member(X, Domain), enum(Tail).
+enum(X)              <=> number(X) | true.
+enum(X), X in Domain <=> member(X, Domain).
 
 % enum_board(Board): fills Board with values
-enum_board([]) <=> true.
-enum_board([ Row | Rows ]) <=>
-    enum(Row),
-    enum_board(Rows).
+%enum_board <=> true.
+
+board(X, Y, Amount, N, E, S, W), enum_board ==>
+    enum(N),
+    enum(E),
+    enum(S),
+    enum(W).
 
 % upto(N, L): L = [1..N]
-upto([], 0).
+upto([], -1).
 upto([ N | L ], N) :-
-    N > 0,
+    N >= 0,
     N1 is N-1,
     upto(L, N1).
 
